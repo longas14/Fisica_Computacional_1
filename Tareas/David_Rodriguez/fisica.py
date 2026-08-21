371e02f3cf09f2f49baa88630aefbdd16669d57a
Samudrp <samueldrp13@gmail.com>
2026-08-20 19:35:43 -0500

    Love Calculator

---
+++ Tareas/David_Rodriguez/fisica.py
@@ -0,0 +1,7 @@
+import random as rm
+
+print("Love Calculator<3")
+x = str(input("Ingrese un nombre: "))
+y = str(input("Ingrese el nombre de la persona: "))
+z = rm.randint(0, 1)
+print("Su porcentaje de amor es: ", z,"%")
