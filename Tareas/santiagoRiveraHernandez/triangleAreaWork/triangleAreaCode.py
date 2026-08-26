e4db56f3d3795918582e5b9bd6465c2ea7818419
santi <santimail177@gmail.com>
2026-08-25 19:30:28 -0500

    Tarea del área del triángulo

---
+++ Tareas/santiagoRiveraHernandez/triangleAreaWork/triangleAreaCode.py
@@ -0,0 +1,5 @@
+print("Este es un archivo python que te calculará el área de un triángulo en función de la base y altura que introduzcas")
+base = float(input("Introduzca el valor de la base del triángulo: "))
+altura = float(input("Introduzca el valor de la altura del triángulo: "))
+area = (1/2) * base * altura
+print(f"El área del triángulo es: {area}")
